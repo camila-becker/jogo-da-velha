66d5f451edbbc36a533434b50cb7e1305d150f1f
Camila Becker <camila.becker01@gmail.com>
2019-10-15 20:11:29 -0300

    Desafio básico - aula 049

---
+++ src/objects/Logo/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+import logoCollabCode from '../../img/logo.svg';
+
+const Logo = () => {
+  return (<img src={logoCollabCode} alt="Logo CollabCode" className="logo"/>);
+};
+
+export default Logo;
