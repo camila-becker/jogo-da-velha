0e679bd588be02774df249abd6a0e8279452cdb7
Camila Becker <camila.becker01@gmail.com>
2019-10-15 19:40:59 -0300

    Aula 049 - Criado o object Sobre

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sobre from './objects/Sobre';
 
 function App() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <a href="#sobre" className="App-link">Sobre</a>
-    </header>
+    <Sobre />
   );
 }
 
