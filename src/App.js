5eda0594f9e59c50367c27f96304d979ae83fe0c
Camila Becker <camila.becker01@gmail.com>
2019-10-16 12:38:02 -0300

    Testando

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import Header from './components/Header'
-import Board from './components/Board';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Board />
-    </>
+    <Header />
   );
 }
 
