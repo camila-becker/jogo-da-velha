e12e04c73f01b7d8ab8976cd2ffd252d584da4d9
Camila Becker <camila.becker01@gmail.com>
2019-10-16 16:17:14 -0300

    Criado o componente hashtag e definido o display grid nele

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
-import Board from './objects/Board';
-import Player from './objects/Player';
+import Hashtag from './components/Hashtag';
+
 function App() {
   return (
     <>
       <Header />
-      <Board>
-        <Player player="x" />
-        <Player player="o" />
-        <Player player="x" />
-
-        <Player player="o" />
-        <Player player="x" />
-        <Player player="o" />
-
-        <Player player="x" />
-        <Player player="o" />
-        <Player player="x" />
-      </Board>
+      <Hashtag />
     </>
   );
 }
