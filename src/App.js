c9cefab1f51aaa6ba442177cff2cdd8ea34e0bba
Camila Becker <camila.becker01@gmail.com>
2019-10-28 09:44:39 -0300

    Desafio basico aula 051

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
-import Hashtag from './components/Hashtag';
+import React from "react";
+import "./App.css";
+import Label from "./objects/Label";
+import Header from "./components/Header";
+import Hashtag from "./components/Hashtag";
 
 const App = () => (
   <main className="app">
     <Header />
     <Hashtag />
+    <Label content="Mostrar Eventos" />
   </main>
 );
 
---
+++ src/objects/Label/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Label = ({ content }) => <label htmlFor="">{content}</label>;
+
+export default Label;
