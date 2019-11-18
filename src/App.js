a81a0e7181061c8e94e592f7a4b7a9c697a61ece
Camila Becker <camila.becker01@gmail.com>
2019-11-18 14:42:24 -0300

    O valor de saber CSS

--- src/App.js
+++ src/App.js
@@ -11,28 +11,22 @@ import HistoryGame from "./components/HistoryGame";
 
 const App = () => {
   const [activeAbout, setActiveAbout] = useState("");
-  const [activeHistoryGame, setHistoryGame] = useState("");
 
   const handleClickAdd = () => setActiveAbout("-active");
 
   const handleClickRemove = () => setActiveAbout("");
-
-  const handleClick = () => {
-    setHistoryGame(old => (old === "-active" ? "" : "-active"));
-  };
 
   return (
     <main id="main" className="app">
       <HeaderGame onClick={handleClickAdd} />
       <HashtagGame />
       <InputCheckbox
-        onClick={handleClick}
         type="checkbox"
         id="show"
         value="show"
         content="Mostrar Eventos"
       />
-      <HistoryGame className={activeHistoryGame} />
+      <HistoryGame />
       <LayerDark className={activeAbout}>
         <HeaderInternal onClick={handleClickRemove} />
         <ProfileUser />
--- src/objects/InputCheckbox/index.js
+++ src/objects/InputCheckbox/index.js
@@ -4,10 +4,10 @@ import "./styles.css";
 import LabelGame from "../LabelGame";
 import InputGame from "../InputGame";
 
-const InputCheckbox = ({ id = "", value = "", content = "", onClick }) => (
+const InputCheckbox = ({ id = "", value = "", content = "" }) => (
   <>
     <InputGame type="checkbox" id={id} value={value} content={content} />
-    <LabelGame onClick={onClick} htmlFor={id} content={content} />
+    <LabelGame htmlFor={id} content={content} />
   </>
 );
 
