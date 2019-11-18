b5664cb8764c8997ff3262c7ea376bf11d01799c
Camila Becker <camila.becker01@gmail.com>
2019-11-18 15:04:04 -0300

    Desafio avançado - aula 053 - Adicionado evento de click no PlayerGame

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -3,13 +3,17 @@ import "./styles.css";
 import playerX from "../../img/playerX.svg";
 import playerO from "../../img/playerO.svg";
 
+const handleClick = () => {
+  console.log("clicou no player");
+};
+
 const PlayerGame = ({ player = false }) => {
   const players = [];
   players["x"] = playerX;
   players["o"] = playerO;
 
   return (
-    <button className="player-game">
+    <button onClick={handleClick} className="player-game">
       {player && <img src={players[player]} alt={`Player ${player}`} />}
     </button>
   );
